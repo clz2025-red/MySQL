31076e38d95bee6f5656618b73167b64676c5d3a
clz2025-red <clz2025.red@gmail.com>
2025-05-28 15:54:53 +0900

    day05

--- Ex/day05.sql
+++ Ex/day05.sql
@@ -109,15 +109,126 @@ inner join jobs j
 ;
 -- -------------------------------------------------
 
+-- ------------------------------
+# left outer join
+-- ------------------------------
+-- 왼쪽 테이블을 기준 ,기준테이블의 정보는 다 보여줌
+-- (Kimberely 가 결과에 있어야 함:소속부서가 없는 직원)
+select	e.first_name,
+		d.department_name,
+        e.department_id,
+        d.department_id
+from employees e 
+left outer join departments d
+on e.department_id = d.department_id
+;
+
+-- ------------------------------
+# right outer join
+-- ------------------------------
+-- 모든 직원의 부서번호, 이름,  부서번호를 출력하세요
+-- 직원이 없는 부서도 표시  (Kimberely 결과에 없다)
+select	e.first_name,
+		d.department_name,
+        e.department_id,
+        d.department_id
+from employees e 
+right outer join departments d
+	on e.department_id = d.department_id
+;
+
+-- ------------------------------
+# right outer join --> left outer join
+-- ------------------------------
+-- 모든 직원의 부서번호, 이름,  부서번호를 출력하세요
+-- 직원이 없는 부서도 표시  (Kimberely 결과에 없다)
+select	e.first_name,
+		d.department_name,
+        e.department_id,
+        d.department_id
+from departments d left outer join  employees e 
+	on d.department_id = e.department_id
+;
+
+-- ------------------------------
+# full outer join    union
+-- ------------------------------
+(-- left조인
+select 	e.employee_id,
+		e.first_name,
+		d.department_id,
+		d.department_name
+from employees e
+left outer join departments d
+	on e.department_id = d.department_id
+)
+union
+(-- right조인
+select 	e.employee_id,
+		e.first_name,
+		d.department_id,
+		d.department_name
+from employees e
+right outer join departments d
+	on e.department_id = d.department_id
+) 
+;
+
+-- 왼쪽기준
+select 	e.employee_id,
+		e.first_name,
+		d.department_id,
+        d.department_name
+from employees e
+left outer join departments d
+	on e.department_id = d.department_id
+;
 
 
+-- 오른쪽기준
+select 	e.employee_id,
+		e.first_name,
+        d.department_id,
+        d.department_name
+from employees e
+right outer join departments d
+	on e.department_id = d.department_id
+;
 
-select count(*) from employees;   -- 107
+-- ------------------------------
+# self join 
+-- ------------------------------
+-- 테이블에서 자신의 pk를 fk로 가지고 있는 경우
+-- 같은 테이블을 두번 불러와서 써야되는 경우
+-- 두번 불러와서 별명을 다르게 준다(꼭 별명을 주어야 함)
+select 	e.employee_id,
+		e.first_name,
+		e.phone_number,
+        e.manager_id,
+        e.department_id,
+        m.first_name,
+        m.phone_number
+from employees e, employees m
+where e.manager_id = m.employee_id
+;
 
-select count(*) from departments;  -- 27
 
-select count(*) from locations;    -- 23
+-- ------------------------------
+# 참고 잘못된 조인
+-- ------------------------------
+-- 아무컬럼이나 where에 사용하면 안된다
+-- 월급과 도시는 아무런 관계가 없다
+select *
+from employees e, locations l
+where e.salary = l.location_id
 
 
-select * from employees;  -- 27
+-- ------------------------------------------------------------------------------------------
+# 조인 연습(equi join, inner join 두가지로 풀어볼것)
+-- 직원아이디, 이름, 월급, 부서아이디, 부서명, 도시아이디, 도시명
+   
+-- 직원아이디, 이름, 월급, 부서아이디, 부서명, 도시아이디, 도시명, 나라아이디, 나라명
+   
+   
+-- 직원아이디, 이름, 월급, 부서아이디, 부서명, 도시아이디, 도시명, 나라아이디, 나라명, 지역아이디, 지역명
 
